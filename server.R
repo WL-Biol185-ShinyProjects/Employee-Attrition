626e6f3340ace31a0348d6194df122169972a680
calames23 <calames23@mail.wlu.edu>
2022-10-19 15:48:46 -0400

    merge commits

--- server.R
+++ server.R
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 server <- function(input, output) {
   set.seed(122)
   histdata <- rnorm(500)
@@ -8,5 +8,3 @@ server <- function(input, output) {
     hist(data)
   })
 }
-=======
->>>>>>> dd06e43792a2cf7799672320d1eb2dd7097b4623
