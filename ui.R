dd06e43792a2cf7799672320d1eb2dd7097b4623
Dylan Walmsley <walmsleyd23@mail.wlu.edu>
2022-10-19 15:33:19 -0400

    basic setup of ui

--- ui.R
+++ ui.R
@@ -1,10 +1,8 @@
 library(shiny)
+library(shinydashboard)
 
-#Application Title 
-titlePanel("Healthcare Employee Attrition")
-
-#Show a plot of the generated distribution 
-mainPanel(
-  plotOutput("Plot"), 
-  selectInput("column", "Choose an X-axis column", choices = colnames())
+dashboardPage(
+  dashboardHeader(),
+  dashboardSidebar(),
+  dashboardBody()
 )
