a10257baf26242876e02f3466a8759257b0bd757
Ellen <duline23@mail.wlu.edu>
2022-10-19 16:16:45 -0400

    fixed merge errors

--- ui.R
+++ ui.R
@@ -2,7 +2,6 @@
 library(shiny)
 library(shinydashboard)
 
-<<<<<<< HEAD
 #Application Title 
 titlePanel("Healthcare Employee Attrition")
 
@@ -12,7 +11,6 @@ mainPanel(
   selectInput("column", "Choose an X-axis column", choices = colnames())
 )
 
-=======
 dashboardPage(
   dashboardHeader(title = "Healthcare Attrition", titleWidth = 750),
   dashboardSidebar(
@@ -35,9 +33,4 @@ dashboardPage(
     )
   )
 )
-<<<<<<< HEAD
->>>>>>> dd06e43792a2cf7799672320d1eb2dd7097b4623
->>>>>>> 69380c63a9950c6b52e4982811524b0bb29e4ad0
-=======
 
->>>>>>> ec6b977ebf4a528808d82ee7171dd5e332271c6d
