5753f68d639566151311c0644887878919420737
calames23 <calames23@mail.wlu.edu>
2022-10-24 15:31:17 -0400

    turned watson_healthcare_modified$Attrition into a factor

--- server.R
+++ server.R
@@ -3,7 +3,7 @@
 
 server <- function(input, output) {
 
-  
+  watson_healthcare_modified$Attrition <- factor(watson_healthcare_modified$Attrition)
   output$densityplot <- renderPlot({
     ggplot(watson_healthcare_modified, aes_string(input$var, fill = watson_healthcare_modified$Attrition)) +
       geom_histogram()  
