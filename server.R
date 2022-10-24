b676263fb219bc8a84a3e0dead328d37e4369ea5
calames23 <calames23@mail.wlu.edu>
2022-10-24 16:09:09 -0400

    changing BusinessTravel to a factor selecting only certain variables for x-variable drop down menu

--- server.R
+++ server.R
@@ -4,9 +4,10 @@
 server <- function(input, output) {
 
 watson_healthcare_modified$Attrition <- factor(watson_healthcare_modified$Attrition)
+watson_healthcare_modified$BusinessTravel <- factor(watson_healthcare_modified$BusinessTravel)
 output$densityplot <- renderPlot({
   ggplot(watson_healthcare_modified, aes_string(input$attrition_data, fill = watson_healthcare_modified$Attrition)) +
-  geom_histogram()  
+  geom_histogram(stat = "count")  
   })
 }
 
