d512dcea320b4113c00202e8e1b9be95abf9f9b3
Ellen <duline23@mail.wlu.edu>
2022-10-24 15:39:53 -0400

    trying to add labels

--- server.R
+++ server.R
@@ -6,7 +6,10 @@ server <- function(input, output) {
   watson_healthcare_modified$Attrition <- factor(watson_healthcare_modified$Attrition)
   output$densityplot <- renderPlot({
     ggplot(watson_healthcare_modified, aes_string(input$var, fill = watson_healthcare_modified$Attrition)) +
-      geom_histogram()  
+      geom_histogram()  +
+      geom_text(aes(label = ..count..), stat = "count", vjust = -.5, colour = "white")
+    
+    
   })
     
     
