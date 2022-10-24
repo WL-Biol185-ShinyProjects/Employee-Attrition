9cfc2949e89fadaa084c40669662baf28c627354
Ellen <duline23@mail.wlu.edu>
2022-10-24 15:23:31 -0400

    test

--- server.R
+++ server.R
@@ -5,8 +5,8 @@ server <- function(input, output) {
 
   
   output$densityplot <- renderPlot({
-    ggplot(watson_healthcare_modified, aes_string(input$var)) +
-      geom_density(adjust = 1.5, alpha = 0.4, fill = 1) 
+    ggplot(watson_healthcare_modified, aes_string(input$var, color = "Attrition") +
+      geom_bar(adjust = 1.5, alpha = 0.4, fill = 1) 
   })
     
     
