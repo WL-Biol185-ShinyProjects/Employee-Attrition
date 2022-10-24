24f3c5a24ef0c31b783e4f5d13bb6adeaefe0614
calames23 <calames23@mail.wlu.edu>
2022-10-24 14:39:27 -0400

    merging for monday class

--- ui.R
+++ ui.R
@@ -1,38 +1,27 @@
 
-library(ggplot2)
-library(dplyr)
-library(tidyr)
 library(shiny)
 library(shinydashboard)
 
-#Application Title 
-titlePanel("Healthcare Employee Attrition")
 
-#Show a plot of the generated distribution 
-mainPanel(
-  plotOutput("Plot"), 
-  selectInput("column", "Choose an X-axis column", choices = colnames())
-)
 
-dashboardPage(skin = green,
-  dashboardHeader(title = "Healthcare Attrition", titleWidth = 750),
-  dashboardSidebar(
-    sidebarMenu(
-      menuItem("Graphs", tabName = "Graphs"),
-      menuItem("History", tabName = "History")
+  dashboardPage(skin = green,
+    dashboardHeader(title = "Healthcare Attrition", titleWidth = 750),
+    dashboardSidebar(
+      sidebarMenu(
+        menuItem("Graphs", tabName = "Graphs"),
+        menuItem("History", tabName = "History")
     )
   ),
-  dashboardBody(
+    dashboardBody(
     #Boxes need to be put in a row (or column)
-    fluidRow(
-      box(plotOutput("plot1"),
-          selectInput("column", "Choose an X-axis column", choices = c("Age", "Monthly Income"))
-      )
-    )
-    
-    tabItems(
-      tabItem(tabName = "Graphs"),
-      tabItem(tabName = "History")
+      fluidRow(
+        box(plotOutput("plot1"),
+          selectInput("column", "Choose an X-axis column", choices = c("Age", "Monthly Income")))
+  ), 
+      tabItems(
+        tabItem(tabName = "Graphs"),
+        tabItem(tabName = "History")
     )
   )
+)
 )
