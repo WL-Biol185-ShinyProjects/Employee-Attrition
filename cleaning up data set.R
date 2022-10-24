1fc0fff8230f293c32442b14c805b4988de4bb0b
Mary Jane McConnell <mcconnellm23@mail.wlu.edu>
2022-10-24 16:13:53 -0400

    committing clean one again

--- cleaning up data set.R
+++ cleaning up data set.R
@@ -4,3 +4,4 @@ library(tidyverse)
 
 watson_healthcare_clean <- watson_healthcare_modified %>% 
   select ( c("Age", "BusinessTravel", "EducationField", "EnvironmentSatisfaction", "Gender", "JobSatisfaction", "MaritalStatus", "MonthlyIncome", "OverTime", "PercentSalaryHike", "TotalWorkingYears", "WorkLifeBalance", "YearsAtCompany", "YearsInCurrentRole"))
+
