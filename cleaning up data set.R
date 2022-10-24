4634de08c2b188dce2986410dca449e97c9a58f1
Mary Jane McConnell <mcconnellm23@mail.wlu.edu>
2022-10-24 15:26:32 -0400

    cleaning up dataset deleted lots of columns

---
+++ cleaning up data set.R
@@ -0,0 +1,5 @@
+#Cleaning up data set 
+library(tidyverse)
+watson_healthcare_modified %>% select ( c("Age", "BusinessTravel", "EducationField", "EnvironmentSatisfaction", "Gender", "JobSatisfaction", "MaritalStatus", "MonthlyIncome", "OverTime", "PercentSalaryHike", "TotalWorkingYears", "WorkLifeBalance", "YearsAtCompany", "YearsInCurrentRole"))
+
+
