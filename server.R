75fa78dc41338351e3d64d1a36f6820ae970f55f
calames23 <calames23@mail.wlu.edu>
2022-10-24 15:37:45 -0400

    cleaning up code changing var tp attrition_data

--- server.R
+++ server.R
@@ -3,12 +3,10 @@
 
 server <- function(input, output) {
 
-  watson_healthcare_modified$Attrition <- factor(watson_healthcare_modified$Attrition)
-  output$densityplot <- renderPlot({
-    ggplot(watson_healthcare_modified, aes_string(input$var, fill = watson_healthcare_modified$Attrition)) +
-      geom_histogram()  
+watson_healthcare_modified$Attrition <- factor(watson_healthcare_modified$Attrition)
+output$densityplot <- renderPlot({
+  ggplot(watson_healthcare_modified, aes_string(input$attrition_data, fill = watson_healthcare_modified$Attrition)) +
+  geom_histogram()  
   })
-    
-    
-  }
+}
 
