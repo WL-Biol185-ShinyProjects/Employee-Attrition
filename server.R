8126ed297267ab5f2ef98b9cbee3015c68ab6e22
Dylan Walmsley <walmsleyd23@mail.wlu.edu>
2022-10-26 14:55:03 -0400

    Need to commit before I can pull

--- server.R
+++ server.R
@@ -1,18 +1,11 @@
 
 # Define server logic required to draw a histogram
-function(input, output) {
-  output$Plot <- renderPlot({
-    
-  })
-}
 
 server <- function(input, output) {
 
-  
-  output$densityplot <- renderPlot({
-    ggplot(watson_healthcare_modified, aes_string(x=input$var)) +
-      geom_density(adjust = 1.5, alpha = 0.4) })
-    
-    
-  }
-
+      watson_healthcare_modified$Attrition <- factor(watson_healthcare_modified$Attrition)
+      output$densityplot <- renderPlot({
+        ggplot(watson_healthcare_modified, aes_string(input$attrition_data, fill = watson_healthcare_modified$Attrition)) +
+        geom_histogram()  
+      })
+}
