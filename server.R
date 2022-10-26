fa226cf4b31887b6295251da3402ffd6419ac91a
calames23 <calames23@mail.wlu.edu>
2022-10-26 15:14:34 -0400

    changing ID names to be different for two different plots

--- server.R
+++ server.R
@@ -6,12 +6,12 @@ server <- function(input, output) {
 watson_healthcare_clean$Attrition <- factor(watson_healthcare_clean$Attrition)
 watson_healthcare_clean$BusinessTravel <- factor(watson_healthcare_clean$BusinessTravel)
 output$histogramplot <- renderPlot({
-  ggplot(watson_healthcare_clean, aes_string(input$attrition_data, fill = watson_healthcare_clean$Attrition)) +
+  ggplot(watson_healthcare_clean, aes_string(input$histogram_data, fill = watson_healthcare_clean$Attrition)) +
     geom_histogram(stat = "count") + 
     ggtitle("Potential Predictors of Healthcare Employee Attrition")})
 output$densityplot <-renderPlot({
- ggplot(watson_healthcare_clean, aes_string(input$attrition_data, fill = watson_healthcare_clean$Attrition)) +
-    geom_density(stat = "density", position = "identity")
+ ggplot(watson_healthcare_clean, aes_string(input$density_data, fill = watson_healthcare_clean$Attrition)) +
+    geom_density()
   })
 }
 
