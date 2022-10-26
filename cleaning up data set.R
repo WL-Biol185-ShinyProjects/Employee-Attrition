96d118ab05892fc4e62d7888a8291e095462e3d3
calames23 <calames23@mail.wlu.edu>
2022-10-25 21:07:44 -0400

    switched data set that we are using from healthcare_clean to watson_healthcare_clean because healthcare_clean had an odd column and app would not run with it. Also selected "Attrition" as a variable in watson_healthcare_clean

--- cleaning up data set.R
+++ cleaning up data set.R
@@ -2,13 +2,10 @@
 library(tidyverse)
 
 watson_healthcare_clean <- watson_healthcare_modified %>% 
-  select ( c("Age", "BusinessTravel", "EducationField", "EnvironmentSatisfaction", "Gender", "JobSatisfaction", "MaritalStatus", "MonthlyIncome", "OverTime", "PercentSalaryHike", "TotalWorkingYears", "WorkLifeBalance", "YearsAtCompany", "YearsInCurrentRole"))
+  select ( c("Age", "BusinessTravel", "EducationField", "EnvironmentSatisfaction", "Gender", "JobSatisfaction", "MaritalStatus", "MonthlyIncome", "OverTime", "PercentSalaryHike", "TotalWorkingYears", "WorkLifeBalance", "YearsAtCompany", "YearsInCurrentRole"
+, "Attrition"))
 
-<<<<<<< HEAD
-write.csv(watson_healthcare_clean, file = "healthcare_clean.csv")
-=======
-
+#write.csv(watson_healthcare_clean, file = "healthcare_clean.csv")
 
 
 
->>>>>>> 7d44f5ab4cf9ea5d840609386bb8af6fdffc47a5
