a2242e8917497156df9a97bc0a6ed137f04b0258
Dylan Walmsley <walmsleyd23@mail.wlu.edu>
2022-10-30 14:48:46 -0400

    Fixing a comma error in one of the input functions

--- ui.R
+++ ui.R
@@ -83,7 +83,7 @@ dashboardPage(skin = "green",
                   selectizeInput("Overtime", "Do You Work Overtime Often?", c("Yes", "No")),
                   sliderInput("PercentSalaryHike", "How Much Has Your Salary Increased over Your Career?", 10, 25, 15),
                   sliderInput("TotalWorkingYears", "How Many Years Have You Worked?", 0, 40, 20, step = 1),
-                  sliderInput("WorkLifeBalance", "How Is Your Work/Life Balance?" 1, 4, 2, step = 1),
+                  sliderInput("WorkLifeBalance", "How Is Your Work/Life Balance?", 1, 4, 2, step = 1),
                   sliderInput("YearsAtCompany", "How Many Years Have You Worked at Your Current Company?", 0, 40, 20, step = 1),
                   sliderInput("YearsInCurrentRole", "How Many Years Have You Worked in Your Current Role?", 0, 20, 10, step = 1),
                   width = 12
