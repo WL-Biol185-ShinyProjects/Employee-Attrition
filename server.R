cfe9dc7a30efd527a6dae2211cec2fc6edf97aee
Ellen <duline23@mail.wlu.edu>
2022-11-07 15:13:53 -0500

    scatterplot

--- server.R
+++ server.R
@@ -44,7 +44,8 @@ server <- function(input, output, session
     {
       ggplot(watson_healthcare_clean, 
            aes_string(input$XScatterData, input$YScatterData)) +
-           geom_point(stat = "identity")
+           geom_point(stat = "identity") +
+           geom_smooth()
      }
                                    )
   
