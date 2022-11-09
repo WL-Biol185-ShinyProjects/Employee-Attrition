9b0e9e180491b875ed10a9c2bbc9eccff4e10a88
Dylan Walmsley <walmsleyd23@mail.wlu.edu>
2022-11-09 15:41:24 -0500

    deleted a SPACE in between select and its parentheses to remove "...1" in clean dataset

--- cleaning up data set.R
+++ cleaning up data set.R
@@ -2,7 +2,7 @@
 library(tidyverse)
 
 watson_healthcare_clean <- watson_healthcare_modified %>% 
-  select ( c( "Age", "BusinessTravel", "EducationField", "EnvironmentSatisfaction", "Gender", "JobSatisfaction", "MaritalStatus", "MonthlyIncome", "OverTime", "PercentSalaryHike", "TotalWorkingYears", "WorkLifeBalance", "YearsAtCompany", "YearsInCurrentRole"
+  select( c( "Age", "BusinessTravel", "EducationField", "EnvironmentSatisfaction", "Gender", "JobSatisfaction", "MaritalStatus", "MonthlyIncome", "OverTime", "PercentSalaryHike", "TotalWorkingYears", "WorkLifeBalance", "YearsAtCompany", "YearsInCurrentRole"
 , "Attrition"))
 
 #write.csv(watson_healthcare_clean, file = "watson_healthcare_clean.csv")
