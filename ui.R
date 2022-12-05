188abd09a1c13857c719fe7de9ee33b200d80110
Dylan Walmsley <walmsleyd23@mail.wlu.edu>
2022-12-04 20:22:37 -0500

    Cleaning up the data in the ui so that the app runs without having the dataset open on rstudio

--- ui.R
+++ ui.R
@@ -3,6 +3,34 @@ library(shinydashboard)
 library(ggplot2)
 library(dplyr)
 library(tidyverse)
+library(readr)
+
+#Importing our dataset so that it opens everytime the app opens
+
+watson_healthcare_modified <- read.csv("watson_healthcare_modified.csv")
+
+watson_healthcare_clean <- watson_healthcare_modified %>% 
+  select( c( "Age", "BusinessTravel", "EducationField", "EnvironmentSatisfaction", "Gender", "JobSatisfaction", "MaritalStatus", "MonthlyIncome", "OverTime", "PercentSalaryHike", "TotalWorkingYears", "WorkLifeBalance", "YearsAtCompany", "YearsInCurrentRole"
+             , "Attrition"))
+
+write.csv(watson_healthcare_clean, file = "watson_healthcare_clean.csv")
+
+watson_healthcare_reg <- watson_healthcare_modified %>% 
+  select ( c( "Age", "BusinessTravel", "EducationField", "EnvironmentSatisfaction", "Gender", "JobSatisfaction", "MaritalStatus", "MonthlyIncome", "OverTime", "PercentSalaryHike", "TotalWorkingYears", "WorkLifeBalance", "YearsAtCompany", "YearsInCurrentRole"
+              , "Attrition"))
+
+write.csv(watson_healthcare_reg, file = "watson_healthcare_reg.csv")
+
+#Creating a dataset so that the MonthlyIncome column can be a categorical variable
+#Allows the attrition estimation function to work
+whc <- watson_healthcare_clean
+write.csv(whc, file = "whc.csv")
+whc$MonthlyIncomeCat <- NA
+whc$MonthlyIncomeCat[whc$MonthlyIncome > 0 & whc$MonthlyIncome < 3999] <- "0-3999"
+whc$MonthlyIncomeCat[whc$MonthlyIncome > 4000 & whc$MonthlyIncome < 7999] <- "4000-7999"
+whc$MonthlyIncomeCat[whc$MonthlyIncome > 8000 & whc$MonthlyIncome < 11999] <- "8000-11999"
+whc$MonthlyIncomeCat[whc$MonthlyIncome > 12000 & whc$MonthlyIncome < 15999] <- "12000-15999"
+whc$MonthlyIncomeCat[whc$MonthlyIncome > 16000 & whc$MonthlyIncome < 20000] <- "16000-20000"
 
 #The general layout of dashboard page contains a dashboardHeader and dashboardSidebar
 dashboardPage(skin = "blue", 
